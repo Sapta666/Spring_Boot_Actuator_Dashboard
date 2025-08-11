c0bc724230a96cc77e17b7c4952ee45435273c1c
Saptarshi Dihidar <dihidar.saptarshi@gmail.com>
2025-08-11 17:20:06 +0530

    prime ng code tested and ui design png file added

--- projects/ng-prime-prac/src/app/app.config.ts
+++ projects/ng-prime-prac/src/app/app.config.ts
@@ -3,10 +3,13 @@ import { provideRouter } from '@angular/router';
 import { provideAnimations } from "@angular/platform-browser/animations";
 
 import { routes } from './app.routes';
+import { ConfirmationService } from 'primeng/api';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimations(),
+
+    ConfirmationService
   ]
 };
--- projects/ng-prime-prac/src/app/prac/prac-01/prac-01.component.ts
+++ projects/ng-prime-prac/src/app/prac/prac-01/prac-01.component.ts
@@ -1,14 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Confirmation, ConfirmationService } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
 import { RippleModule } from 'primeng/ripple';
+import { DialogModule } from 'primeng/dialog';
 
 @Component({
   selector: 'app-prac-01',
   standalone: true,        
-  imports: [ButtonModule, RippleModule],
+  imports: [ButtonModule, RippleModule, DialogModule],
   templateUrl: './prac-01.component.html'
 })
 export class Prac01Component {
+
+  //#region Variables
+
+  //#endregion
+
+  //#region Page Load
+
+  constructor(
+    private _confirmationService: ConfirmationService
+  ) {
+
+  }
+
+  //#endregion
+
+  //#region Component Functions
+
+  protected onSubmitClick(): void {
+    let tempEventEmt: EventEmitter<any> = new EventEmitter(); 
+
+    let confirmation: Confirmation = {
+      message: "Submit clicked",
+      header:  "Confirm Dialog Testing",
+      key: "dialog-two",
+      accept: () => {
+        alert("accepted")
+      },
+      reject: () => {
+        alert("reject")
+      },
+
+      acceptEvent: tempEventEmt
+    };    
+
+    setTimeout(() => {
+      tempEventEmt.emit("testing");
+    },3000);
+
+    tempEventEmt.subscribe(value => {
+      console.log(value);
+    });
+
+    this._confirmationService.confirm(confirmation);
+  }
+
+  //#endregion
   
 }
