5eca609d48f0903dac40e047c5ca9d57c277fdf1
Saptarshi Dihidar <dihidar.saptarshi@gmail.com>
2025-08-12 20:57:50 +0530

    top menu design implemented with responsive features

---
+++ src/app/common/enums/breakpoints.enum.ts
@@ -0,0 +1,7 @@
+export enum BreakpointsEnum {
+    "sm" = 425,
+    "md" = 576,
+    "lg" = 768,
+    "xl" = 1024,
+    "xxl" = 1200
+}
--- src/app/components/top-menu/top-menu.component.ts
+++ src/app/components/top-menu/top-menu.component.ts
@@ -1,24 +1,29 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { AccordionModule } from 'primeng/accordion';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { PanelModule } from 'primeng/panel';
 import { ButtonModule } from 'primeng/button';
 import { RippleModule } from 'primeng/ripple';
+import { BreakpointsEnum } from '../../common/enums/breakpoints.enum';
 
 
 @Component({
   selector: 'app-top-menu',
   standalone: true,
-  imports: [AccordionModule, ButtonModule, RippleModule,
+  imports: [PanelModule, ButtonModule, RippleModule,
       CommonModule
   ],
   templateUrl: './top-menu.component.html'
 })
-export class TopMenuComponent {
+export class TopMenuComponent implements OnInit {
   
   //#region Variables
 
-  protected activeIndex = -1;
-  protected isHeaderDropdownDisabled: boolean = true;
+  private _previousWidth: number = 500;
+
+  protected isPanelToggleable: boolean = false;
+  protected isDropdownOpened: boolean = false;
+
+  protected breakpointEnum: typeof BreakpointsEnum = BreakpointsEnum;
 
   //#endregion
 
@@ -30,6 +35,35 @@ export class TopMenuComponent {
 
   constructor() {
 
+  }
+
+  ngOnInit(): void {
+    this._previousWidth = window.innerWidth;
+     if(window.innerWidth <= BreakpointsEnum.xxl) {      
+      this.isPanelToggleable = true;
+      this.isDropdownOpened = false;
+    }
+  }
+
+  @HostListener("window:resize")
+  public noResize() {
+    if(window.innerWidth <= BreakpointsEnum.xxl && this._previousWidth > BreakpointsEnum.xxl) {      
+      this.isPanelToggleable = true;
+      this.isDropdownOpened = false;
+    } else if(window.innerWidth > BreakpointsEnum.xxl) {
+      this.isPanelToggleable = false;
+      this.isDropdownOpened = false;
+    }
+
+    this._previousWidth = window.innerWidth;
+  }
+
+  //#endregion
+
+  //#region Component Functions
+
+  protected onDropDownToggleClick(): void {
+    this.isDropdownOpened = !this.isDropdownOpened;
   }
 
   //#endregion
